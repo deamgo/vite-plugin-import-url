4106495ec8dfb866d920e75a5e433b29865cac2f
zypbk <102448898+zypbk@users.noreply.github.com>
2024-06-18 18:38:28 +0800

    feat: add vite-plugin-import-url (#1)
    - During the transformer build phase, hijacking? Change the code
    returned by the URL to the correct path
    - In the generateBundle stage, use the plugins within vite to package
    the files using rollback and place them in the dist file
    - Support importing URL with {type: URL} mode

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,146 @@
-import { rollup, type RollupBuild } from "rollup";
 import { type Plugin, type ResolvedConfig } from "vite";
+import { rollup, type RollupBuild } from "rollup";
+import { existsSync } from "fs";
+import path, { extname } from "path";
+import { readFile } from "node:fs/promises";
+import postcss from "rollup-plugin-postcss";
 
-let configResolved: ResolvedConfig | undefined;
-const entryFiles = new Set<{ code: string; id: string }>();
+export default function viteImportUrl(): Plugin {
+  let configResolved: ResolvedConfig;
+  const supportFileType = [".js", ".ts", ".tsx", ".jsx"];
+  const entryFiles = new Set<{
+    code: string;
+    id: string | undefined;
+    type: "virtual" | "url";
+  }>();
 
-export default function importUrl(): Plugin {
+  function urlImportRegex(str: string) {
+    return new RegExp(str + `['"] *(?:with) *{ *type: *['"]url['"]`);
+  }
+
+  const virtualModules = new Map<string, string>();
   return {
     name: "vite-plugin-import-url",
-    apply: "build",
-
+    enforce: "pre",
     configResolved(config: ResolvedConfig) {
       configResolved = config;
     },
+    async resolveId(source, importer, options) {
+      if (importer && existsSync(importer)) {
+        let modId;
+        const ext = extname(importer);
+        if (supportFileType.includes(ext)) {
+          const resolvedId = (await this.resolve(source, importer, options))
+            ?.id;
+          if (!resolvedId || !existsSync(resolvedId)) {
+            return;
+          }
+          const importerContent = await readFile(importer!, {
+            encoding: "utf8",
+          });
 
-    async transform(code: string, id: string) {
-      if (id.endsWith("?url") && configResolved?.command === "build") {
-        const regex = /\/([^\/]+)\.tsx?(?=\?|$)/;
-        const match = id.match(regex);
-        if (match && match[1]) {
-          const url = `${configResolved?.base || "/"}${match[1]}.js`;
-          const newCode = `const url = '${url}'; export default url;`;
-          entryFiles.add({ code, id });
-          return {
-            code: newCode,
-          };
+          const regexp = urlImportRegex(source);
+          if (regexp.test(importerContent)) {
+            modId = "\0virtual:" + source + ext;
+            virtualModules.set(modId, resolvedId);
+          }
         }
+        return modId;
       }
-      return null;
+    },
+    async load(id: string) {
+      const resolvedId = Array.from(virtualModules.entries()).find(
+        ([key, value]) => value === id
+      )?.[0];
+      if (virtualModules.has(id) || resolvedId) {
+        const realId = virtualModules.get(resolvedId || id)!;
+        this.addWatchFile(realId);
+        let createCode = "";
+        const relativePath = path.relative(configResolved.root, realId);
+        createCode = `const url = '${relativePath}'; export default url;`;
+        return createCode;
+      }
+    },
+    async transform(code: string, id: string) {
+      const ext = extname(id);
+      const resolvedId = Array.from(virtualModules.entries()).find(
+        ([key, value]) => value === id
+      )?.[0];
+
+      if (
+        (id.includes("virtual") && supportFileType.includes(ext)) ||
+        id.endsWith("?url") ||
+        resolvedId
+      ) {
+        let newId = resolvedId || id;
+        let newCode = code;
+        if (configResolved?.command === "build") {
+          const regex = /\/([^\/]+)\.tsx?(?=\?|$)/;
+          const match = newId.match(regex);
+
+          if (match && match[1]) {
+            const url = `${configResolved?.base || "/"}${match[1]}.js`;
+            if (newId.includes("virtual")) {
+              const realId = virtualModules.get(newId);
+              newCode = `const url = '${url}'; export default url;`;
+              entryFiles.add({ code, id: realId, type: "virtual" });
+            } else {
+              newCode = `const url = '${url}'; export default url;`;
+              entryFiles.add({ code, id, type: "url" });
+            }
+          }
+        }
+
+        if (configResolved.command === "serve") {
+          newCode = code;
+        }
+
+        return {
+          code: newCode,
+        };
+      }
     },
 
     async generateBundle() {
       if (configResolved?.command === "build") {
         const plugins = configResolved?.worker?.plugins;
+        const outputDir = configResolved.build.outDir || "dist";
+
         entryFiles.forEach(async (file) => {
-          const actualPath = file.id.slice(0, -4);
+          let actualPath;
+          switch (file.type) {
+            case "url":
+              actualPath = file.id?.slice(0, -4);
+              break;
+            case "virtual":
+              actualPath = file.id;
+          }
+          if (!actualPath) {
+            return;
+          }
+          const noNeedVitePlugins = ["vite:css", "vite:css-post"];
+          const vitePlugins = (await plugins([])).filter(
+            (i) => !noNeedVitePlugins.includes(i.name)
+          );
           const bundle: RollupBuild = await rollup({
             input: actualPath,
-            plugins: plugins ? await plugins([actualPath]) : [],
+            plugins: [
+              // @ts-ignore
+              postcss({
+                inject: true,
+                extract: false,
+              }),
+              vitePlugins,
+            ],
             preserveEntrySignatures: "strict",
           });
-          await bundle.write({
-            dir: "dist",
+          await bundle.generate({
             format: "esm",
-            assetFileNames: "[name].css",
+          });
+
+          bundle.write({
+            dir: outputDir,
+            format: "esm",
           });
           await bundle.close();
         });
